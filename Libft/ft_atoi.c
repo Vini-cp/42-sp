54f9d4c6f8f8230bf4022921f3e8b3253e461b7e
ViniciusCordeiroP <viniciuscordeirop@gmail.com>
2021-02-06 16:54:46 -0300

    Deleted a folder and add makefile bonus part

--- Libft/ft_atoi.c
+++ Libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: vcordeir <vcordeir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 21:53:57 by vcordeir          #+#    #+#             */
-/*   Updated: 2021/02/02 21:54:03 by vcordeir         ###   ########.fr       */
+/*   Updated: 2021/02/06 16:47:12 by vcordeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,21 @@ int		ft_atoi(const char *str)
 	int i;
 	int mult;
 	int res;
+	int bool_first;
 
 	i = 0;
-	mult = 1;
 	res = 0;
+	mult = 1;
+	bool_first = 1;
 	while (str[i] != '\0')
 	{
-		if (str[i] == '-' && mult == 1)
+		if (str[i] == '-' && bool_first)
+		{
 			mult = -1;
+			bool_first = 0;
+		}
+		else if (str[i] == '+' && bool_first)
+			bool_first = 0;
 		else if (str[i] >= '0' && str[i] <= '9')
 		{
 			while (str[i] >= '0' && str[i] <= '9')
@@ -32,7 +39,7 @@ int		ft_atoi(const char *str)
 			}
 			return (mult * res);
 		}
-		else
+		else if ((str[i] < 7 || str[i] > 13) && str[i] != 32)
 			return(0);
 		i++;
 	}
