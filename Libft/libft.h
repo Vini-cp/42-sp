020bc53d492fac464877c722d26f83edf571f122
ViniciusCordeiroP <viniciuscordeirop@gmail.com>
2021-02-10 23:08:28 -0300

    Norminette libft.h 5

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,12 +6,12 @@
 /*   By: vcordeir <vcordeir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/10 22:13:13 by vcordeir          #+#    #+#             */
-/*   Updated: 2021/02/10 23:07:49 by vcordeir         ###   ########.fr       */
+/*   Updated: 2021/02/10 23:08:22 by vcordeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef		LIBFT_H_
-#define		LIBFT_H_
+#ifndef LIBFT_H_
+#define LIBFT_H_
 
 #include <stddef.h>
 #include <stdlib.h>
