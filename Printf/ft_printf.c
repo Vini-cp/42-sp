6e28771fcdb0961df641280d8b550acd813acc40
ViniciusCordeiroP <viniciuscordeirop@gmail.com>
2021-03-04 10:23:53 -0300

    V0 printf

---
+++ Printf/ft_printf.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vcordeir <vcordeir@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/26 23:13:06 by vcordeir          #+#    #+#             */
+/*   Updated: 2021/03/04 10:23:17 by vcordeir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libftprintf.h"
+#include <stdio.h>
+
+int	ft_printf(const char *, ...)
+{
+    va_list ap;
+    int d;
+    char c, *s;
+
+   va_start(ap, fmt);
+    while (*fmt)
+        switch (*fmt++) {
+        case 's':              /* string */
+            s = va_arg(ap, char *);
+            printf("string %s\n", s);
+            break;
+        case 'd':              /* int */
+            d = va_arg(ap, int);
+            printf("int %d\n", d);
+            break;
+        case 'c':              /* char */
+            /* need a cast here since va_arg only
+               takes fully promoted types */
+            c = (char) va_arg(ap, int);
+            printf("char %c\n", c);
+            break;
+        }
+    va_end(ap);
+}
